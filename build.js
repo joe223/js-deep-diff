c023e51bf0da317adba4fa063d8f41f0272cd417
HeZaichang.WeRDyin <heyin223@163.com>
2018-12-19 20:30:28 +0800

    1.0.0

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ const path = require('path')
 const ncc = require('@zeit/ncc')
 const gzipsize = require('gzip-size')
 const file = path.resolve(process.cwd(), './diff.js')
+const dist = path.resolve(process.cwd(), './dist/index.js')
 
 build()
 fs.watchFile(file, () => {
@@ -12,8 +13,9 @@ fs.watchFile(file, () => {
 function build () {
   ncc(file, {
     minify: true,
-    sourceMap: true
+    sourceMap: false
   }).then(({ code }) => {
+    fs.writeFileSync(dist, code)
     console.log(new Date().toLocaleString(), 'build success, ', `Gzip size: ${gzipsize.sync(code) / 10e2}kb`)
   }).catch(err => {
     console.error(err)
